9fd75c081b7689699c7588a26544db0d3ca4ec37
Andrew Kaiser <ndrwksr@gmail.com>
2020-03-16 19:45:40 -0500

    Added endpoint, bolstered documentation, fixed bugs. Stable!

--- README.md
+++ README.md
@@ -7,14 +7,98 @@ The current architecture of this system is "push" not "pull", meaning that other
 to put new documents in the database or update their retraction flag, and nothing further. The Firestore database is
 never queried, and primarily serves as a highly scalable audit log.
 
+Exposures can be added to the database either through a Firebase SDK for the appropriate platform, or via the `webApi`
+function's `POST /exposures` endpoint.
+
 ## Functions
-#### `messageOnNewExposure`
+### `webApi`
+This function hooks in Express, see Endpoints for further details.
+
+### `messageOnNewExposure`
 This function is invoked when a new document is created in the `/exposures` collection. It sends a message to the new
 `Exposure`'s location fingerprint, meaning all users who have scanned that location will receive a push notification
 in under a second after the `Exposure` document is created.
 
-#### `messageOnRetractedExposure`
+### `messageOnRetractedExposure`
 Because we all make mistakes, if an `Exposure` document is created on accident and now should be retracted to avoid
 unnecessary panic, this function offers an easy way to issue a retraction message to all relevant users. The function
 is invoked upon any update to the `/exposures` collection, and if the retraction flag on the new `Exposure` is set,
 the retraction message is sent. If said flag is unset, this function is a NOOP.
+
+## Endpoints
+All endpoints are under the path `/webApi`, and the host is `https://us-central1-zerobase-notifications.cloudfunctions.net`
+
+### `/exposures`
+#### `POST`
+Creates a new `Exposure` document in the `/exposures` collection.
+
+##### Sample Request:
+```json
+HOST: https://us-central1-zerobase-notifications.cloudfunctions.net/webApi/exposures 
+
+BODY:   
+{
+  "intersected_at":"2020-12-18T16:39:57-08:00",
+  "intersection_device_fp":"topic",
+  "retracted":false,
+  "severity":"TESTED_POSITIVE"
+}
+```  
+
+##### Sample Response:
+```json
+STATUS: 201
+
+BODY:
+{
+  "_id": "bk3Ezydl48KksL32KiX2",
+  "intersected_at": "2020-12-19T00:39:57.000Z",
+  "intersection_device_fp": "topic",
+  "retracted": false,
+  "severity": "TESTED_POSITIVE"
+}
+```
+
+##### Expected Errors:
+
+Invalid Request Body (missing retracted)
+```json
+REQUEST BODY:
+{
+  "intersected_at":"2020-12-18T16:39:57-08:00",
+  "intersection_device_fp":"topic",
+  "severity":"TESTED_POSITIVE"
+}
+
+RESPONSE BODY:
+{
+  "message": "Invalid Request",
+  "data": {
+    "intersected_at": "2020-12-18T16:39:57-08:00",
+    "intersection_device_fp": "topic",
+    "severity": "TESTED_POSITIVE"
+  },
+  "error": {
+    "isJoi": true,
+    "name": "ValidationError",
+    "details": [
+      {
+        "message": "\"retracted\" is required",
+        "path": [
+           "retracted"
+        ],
+        "type": "any.required",
+        "context": {
+          "key": "retracted",
+          "label": "retracted"
+        }
+      }
+    ],
+    "_object": {
+      "intersected_at": "2020-12-18T16:39:57-08:00",
+      "intersection_device_fp": "topic",
+      "severity": "TESTED_POSITIVE"
+    }
+  }
+}
+```
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,132 +1,93 @@
 import * as functions from 'firebase-functions';
-import * as admin from 'firebase-admin';
-import {DocumentSnapshot} from "firebase-functions/lib/providers/firestore";
+import * as admin from "firebase-admin";
+import * as express from 'express';
+import {_messageOnNewExposure, _messageOnRetractedExposure} from "./firebase_funcs";
+import {Exposure} from './models/exposure';
 
-admin.initializeApp();
+const Joi = require('@hapi/joi');
+const cors = require('cors');
+
+admin.initializeApp(functions.config().firebase);
 
 const fcm = admin.messaging();
 
-/**
- * An enum for the possible severities of an Exposure.
- * These details haven't been ironed out as of 3/16/20.
- */
-enum Severity {
-    SUSPECTED,
-    TESTED_POSITIVE
-}
+const app = express();
+// https://itnext.io/building-a-serverless-restful-api-with-cloud-functions-firestore-and-express-f917a305d4e6
+const main = express(); // I can't explain why, but we can't use app in place of main. I tried.
+
+main.use('/api/v1', app);
+main.use(cors());
 
 /**
- * A class to represent a document in the exposures collection.
+ * Creates a new Exposure in the /exposures collection.
+ * messageOnNewExposure is invoked via the platform, so we don't need to call it at the end.
+ *
+ * Sample Request:
+   HOST: https://us-central1-zerobase-notifications.cloudfunctions.net/webApi/exposures
+   {
+	"intersected_at":"2020-12-18T16:39:57-08:00",
+	"intersection_device_fp":"topic",
+	"retracted":false,
+	"severity":"TESTED_POSITIVE"
+   }
+
+ * Sample Response:
+   STATUS: 201
+   {
+    "_id": "bk3Ezydl48KksL32KiX2",
+    "intersected_at": "2020-12-19T00:39:57.000Z",
+    "intersection_device_fp": "topic",
+    "retracted": false,
+    "severity": "TESTED_POSITIVE"
+   }
+
+ * Planned Error Responses:
+   STATUS: 422
+   {
+    "message": "[Human-Readable Message]"
+    "error": "[Insert Error Here]"
+   }
  */
-class Exposure {
-    /**
-     * The time at which the document was created.
-     */
-    created_at: Date;
-
-    /**
-     * The time at which the exposed party intersected with the infected individual.
-     */
-    intersected_at: Date;
-
-    /**
-     * The device fingerprint representing the location at which the parties intersected.
-     */
-    intersection_device_fp: string;
-
-    /**
-     * True if the exposure has been retracted (such as if it was sent in error). False otherwise.
-     */
-    retracted: boolean;
-
-    /**
-     * The severity of the exposure. See Severity.
-     */
-    severity: Severity;
-
-    /**
-     * Builds a new Exposure from a document snapshot.
-     * @param snapshot The snapshot from which to build the new Exposure.
-     */
-    constructor(snapshot: DocumentSnapshot) {
-        this.created_at = snapshot.get('created_at');
-        this.intersected_at = snapshot.get('intersected_at');
-        this.intersection_device_fp = snapshot.get('intersection_device_fp');
-        this.retracted = snapshot.get('retracted');
-        this.severity = snapshot.get('severity')
+// TODO [ndrwksr | 3/16/20]: This is the wrong place to put this, but the question "where should I put this" isn't
+//  one I can answer. If you know better how to structure this, please feel free to fix it.
+main.post('/exposures', async (req, res) => {
+    const validated = Joi.validate(req.body, Exposure.schema);
+    if (validated.error) {
+        // Failed validation
+        res.status(422).send({
+            message: 'Invalid Request',
+            data: req.body,
+            error: validated.error
+        });
+    } else {
+        // TODO [ndrwksr | 3/16/20]: Converting req.body into an Exposure, then extracting the data like this is
+        //  redundant but it doesn't work if 'exposure.data()' is replaced with 'req.body'.
+        //  This is almost certainly an easy fix, I'm just not experienced enough with JS to figure these ones out
+        const exposure = Exposure.fromReqBody(req.body);
+        admin.firestore().collection('exposures').add(exposure.data())
+            .then(ref => {
+                console.log("Created new Exposure. ID: " +  ref.id);
+                res.status(201).send({
+                    _id: ref.id,
+                    ...validated.value
+                })
+            })
+            .catch(e => {
+                console.log(e);
+                res.status(500).send({
+                    message: 'Could Not Save New Exposure',
+                    data: req.body,
+                    error: e
+                });
+            })
     }
-}
+});
 
-/**
- * Sends a message to all who have subscribed to the exposure location's FP topic
- * when a new Exposure is added to the exposure collection.
- */
-export const messageOnNewExposure = functions.firestore
-    .document('exposures/{exposureId}')
-    .onCreate(async snapshot => {
-        const exposure = new Exposure(snapshot);
-        const time = getPrettyTime(exposure.intersected_at);
+// noinspection JSUnusedGlobalSymbols -- Used by firebase (ndrwksr)
+export const webApi = functions.https.onRequest(main);
 
-        return sendToTopic(
-            'New CoV-SARS-2 Exposure on ' + time,
-            'Don\'t panic! We\'re here to help. Tap here.',
-            exposure.intersection_device_fp
-        )
-    });
+// noinspection JSUnusedGlobalSymbols -- Used by firebase (ndrwksr)
+export const messageOnNewExposure = _messageOnNewExposure(fcm);
 
-/**
- * Sends a message to all who have subscribed to the exposure location's FP topic
- * when an Exposure has been retracted (such as if it were sent in err).
- */
-export const messageOnRetractedExposure = functions.firestore
-    .document('exposures/{exposureId}')
-    .onUpdate((change) => {
-        const newExposure = new Exposure(change.after);
-        const time = getPrettyTime(newExposure.intersected_at);
-
-        if (newExposure.retracted) {
-            return sendToTopic(
-                'Retracted CoV-SARS-2 Exposure on ' + time,
-                'Still think you\'re sick? Tap here.',
-                newExposure.intersection_device_fp
-            )
-        } else {
-            return
-        }
-    });
-
-/**
- * Formats a Date in to be short and human-readable.
- * @param date The date to format.
- */
-function getPrettyTime(date: Date) {
-    const dateTimeFormat = new Intl.DateTimeFormat('en', {year: 'numeric', month: 'short', day: '2-digit'});
-    const [{value: month}, {value: day}, {value: year}] = dateTimeFormat.formatToParts(date);
-    return month + " " + day + " " + year;
-}
-
-/**
- * Sends a message on the specified topic.
- * @param title The title for the new message.
- * @param body The body for the new message.
- * @param topic The topic on which to send the message. This will usually be the location FP
- *              for the location of exposure, but could also be something like "general" to
- *              send a message to all users of the app.
- */
-function sendToTopic(title: string, body: string, topic: string) {
-    const payload: admin.messaging.MessagingPayload = {
-        notification: {
-            title: title,
-            body: body,
-            icon: 'https://zerobase.io/dist/img/zerobase_medium_logo.png',
-            click_action: 'FLUTTER_NOTIFICATION_CLICK' // required only for onResume or onLaunch callbacks
-        },
-        // TODO [ndrwksr | 3/16/20]: Implement adding payloads so that the app can determine
-        //  how to appropriately react to the message.
-        // data: {
-        //
-        // }
-    };
-
-    return fcm.sendToTopic(topic, payload);
-}
+// noinspection JSUnusedGlobalSymbols -- Used by firebase (ndrwksr)
+export const messageOnRetractedExposure = _messageOnRetractedExposure(fcm);
