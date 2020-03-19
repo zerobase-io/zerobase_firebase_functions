66e1d61d1ab0742e3d5ad599cf19b728a0ff3662
Andrew Kaiser <ndrwksr@gmail.com>
2020-03-19 09:21:21 -0500

    Most of the way through rewriting the push notification system. UNSTABLE!

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,8 @@
 import * as functions from 'firebase-functions';
 import * as admin from "firebase-admin";
 import * as express from 'express';
+import {acceptNewMessageRequest} from "./messaging";
+import {MessagingRequest} from "./models/messagingRequest";
 
 const cors = require('cors');
 
@@ -10,6 +12,8 @@ const app = express();
 // https://itnext.io/building-a-serverless-restful-api-with-cloud-functions-firestore-and-express-f917a305d4e6
 const main = express(); // I can't explain why, but we can't use app in place of main. I tried.
 
+const fcm = admin.messaging();
+
 main.use('/api/v1', app);
 main.use(cors());
 
@@ -17,8 +21,14 @@ main.use(cors());
 //  - This is the wrong place to put this, but the question "where should I put this" isn't
 //    one I can answer. If you know better how to structure this, please feel free to fix it.
 main.post('/messaging', async (req, res) => {
-    // TODO [ndrwksr | 3/18/20]: See https://github.com/zerobase-io/zerobase_firebase_functions/pull/9
-    //  - Impl according to updated README.md from above pull request
+    console.log("POST /messaging", req.body);
+    //let request = MessagingRequest.fromReqBody(req.body);
+    res.send("ok")
+    // acceptNewMessageRequest(fcm)(request)
+    //     .then(response => res.status(201).send(response))
+    //     .catch(e => res.status(500).send({
+    //         error: e
+    //     }))
 });
 
 // noinspection JSUnusedGlobalSymbols -- Used by firebase (ndrwksr | 3/16/20)
---
+++ functions/src/messaging.ts
@@ -0,0 +1,145 @@
+import * as admin from "firebase-admin";
+import {MessagingRequest} from "./models/messagingRequest";
+import {MessagingMedium} from "./models/messagingMedium";
+import {MessagingResponse} from "./models/messagingResponse"
+import {MessagingOutcome} from "./models/messagingOutcome"
+import {
+    getEmailBodyForLocale,
+    getEmailSubjectForLocale,
+    getPnBodyForLocale,
+    getPnTitleForLocale,
+    getSmsMessageForLocale
+} from "./i18n/messageProvider";
+
+export function acceptNewMessageRequest(fcm: admin.messaging.Messaging): (message: MessagingRequest) => Promise<MessagingResponse> {
+    return function (message: MessagingRequest): Promise<MessagingResponse> {
+        const deviceFp = message.contactDetails.get(MessagingMedium.DEVICE_FP);
+        let pnPromise: Promise<MessagingOutcome> | undefined = undefined;
+        if (deviceFp) {
+            pnPromise = sendToTopic(
+                getPnTitleForLocale(message.messageType, message.locale),
+                getPnBodyForLocale(message.messageType, message.locale),
+                deviceFp,
+                fcm
+            )
+        }
+
+
+        const emailAddress = message.contactDetails.get(MessagingMedium.EMAIL);
+        let emailPromise: Promise<MessagingOutcome> | undefined = undefined;
+        if (emailAddress) {
+            emailPromise = sendEmail(
+                getEmailSubjectForLocale(message.messageType, message.locale),
+                getEmailBodyForLocale(message.messageType, message.locale),
+                emailAddress
+            )
+        }
+
+        const phoneNumber = message.contactDetails.get(MessagingMedium.PHONE_NUMBER);
+        let smsPromise: Promise<MessagingOutcome> | undefined = undefined;
+        if (phoneNumber) {
+            smsPromise = sendSms(
+                getSmsMessageForLocale(message.messageType, message.locale),
+                phoneNumber
+            )
+        }
+
+        return Promise.all([pnPromise, smsPromise, emailPromise]).then(values => {
+            let pnOutcome = values[0];
+            let emailOutcome = values[1];
+            let smsOutcome = values[2];
+
+            let messagingOutcomes = new Map<MessagingMedium, MessagingOutcome>();
+            if (pnOutcome) {
+                console.log("Attempted push notification:", pnOutcome);
+                messagingOutcomes.set(MessagingMedium.DEVICE_FP, pnOutcome)
+            }
+
+            if (emailOutcome) {
+                console.log("Attempted email:", emailOutcome);
+                messagingOutcomes.set(MessagingMedium.DEVICE_FP, emailOutcome)
+            }
+
+            if (smsOutcome) {
+                console.log("Attempted SMS:", smsOutcome);
+                messagingOutcomes.set(MessagingMedium.DEVICE_FP, smsOutcome)
+            }
+
+            let response = new MessagingResponse(
+                undefined,
+                message.messageType,
+                message.locale,
+                message.contactDetails,
+                messagingOutcomes
+            );
+
+            return admin.firestore().collection("/messaging").add(response).then(ref => {
+                response._id = ref.id;
+                return response;
+            })
+        })
+    }
+}
+
+/**
+ * Sends a message on the specified topic.
+ * @param title The title for the new message.
+ * @param body The body for the new message.
+ * @param topic The topic on which to send the message. This will usually be the location FP
+ *              for the location of exposure, but could also be something like "general" to
+ *              send a message to all users of the app.
+ * @param fcm The Messaging instance to send the messages with.
+ */
+function sendToTopic(
+    title: string,
+    body: string,
+    topic: string,
+    fcm: admin.messaging.Messaging
+): Promise<MessagingOutcome> {
+    console.log("sendToTopic", title, body, topic);
+    const payload: admin.messaging.MessagingPayload = {
+        notification: {
+            title: title,
+            body: body,
+            icon: 'https://zerobase.io/dist/img/zerobase_medium_logo.png',
+            click_action: 'FLUTTER_NOTIFICATION_CLICK' // required only for onResume or onLaunch callbacks
+        },
+        // TODO [ndrwksr | 3/16/20]: See https://github.com/zerobase-io/zerobase_firebase_functions/issues/2
+        //  - Implement adding payloads so that the app can determine
+        //    how to appropriately react to the message.
+        // data: {
+        //
+        // }
+    };
+
+    return new Promise(resolve => {
+        fcm.sendToTopic(topic, payload).then(messagingResponse => {
+            if (messagingResponse) {
+                resolve(new MessagingOutcome(true, undefined))
+            } else {
+                resolve(new MessagingOutcome(false, "fcm.sendToTopic returned undefined"))
+            }
+        }).catch(e => {
+            resolve(new MessagingOutcome(false, e))
+        })
+    });
+}
+
+function sendEmail(
+    subject: string,
+    body: string,
+    emailAddress: string
+): Promise<MessagingOutcome> {
+    // NO REJECTIONS! Resolve with a MessagingOutcome instead.
+    console.log("sendEmail:", subject, body, emailAddress);
+    return Promise.resolve(new MessagingOutcome(true, undefined))
+}
+
+function sendSms(
+    text: string,
+    phoneNumber: string
+): Promise<MessagingOutcome> {
+    // NO REJECTIONS! Resolve with a MessagingOutcome instead.
+    console.log("sendSms:", text, phoneNumber);
+    return Promise.resolve(new MessagingOutcome(true, undefined))
+}
