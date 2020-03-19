c8da2e12c51c149ef7a8c66b5b44ece8abca853b
Andrew Kaiser <ndrwksr@gmail.com>
2020-03-19 17:38:17 -0500

    Quick changes to handling rejections, added comments

--- functions/src/messaging.ts
+++ functions/src/messaging.ts
@@ -11,8 +11,20 @@ import {
     getSmsMessageForLocale
 } from "./i18n/messageProvider";
 
+/**
+ * Returns a function that processes MessagingRequests and sends the messages outline in the request.
+ * @param fcm The messaging provider to use to send push notifications.
+ */
 export function acceptNewMessageRequest(fcm: admin.messaging.Messaging): (message: MessagingRequest) => Promise<MessagingResponse> {
+    /**
+     * Returns a Promise<MessagingResponse> with the results of the system's attempts to honour the MessagingRequest.
+     * @param message The MessagingRequest containing required contact details and type of message to be sent.
+     */
     return function (message: MessagingRequest): Promise<MessagingResponse> {
+        // TODO [ndrwksr | 3/19/20]: This is an ideal candidate for some refactoring to make adding more messaging
+        //  mediums easier and put some of this functionality behind interfaces.
+
+        // Send push notification
         const deviceFp = message.contactDetails[MessagingMedium.DEVICE_FP];
         let pnPromise: Promise<MessagingOutcome> | undefined = undefined;
         if (deviceFp) {
@@ -24,6 +36,7 @@ export function acceptNewMessageRequest(fcm: admin.messaging.Messaging): (messag
             )
         }
 
+        // Send email
         const emailAddress = message.contactDetails[MessagingMedium.EMAIL];
         let emailPromise: Promise<MessagingOutcome> | undefined = undefined;
         if (emailAddress) {
@@ -31,18 +44,20 @@ export function acceptNewMessageRequest(fcm: admin.messaging.Messaging): (messag
                 getEmailSubjectForLocale(message.messageType, message.locale),
                 getEmailBodyForLocale(message.messageType, message.locale),
                 emailAddress
-            )
+            ).catch(e => new MessagingOutcome(false, e))
         }
 
+        // Send SMS
         const phoneNumber = message.contactDetails[MessagingMedium.PHONE_NUMBER];
         let smsPromise: Promise<MessagingOutcome> | undefined = undefined;
         if (phoneNumber) {
             smsPromise = sendSms(
                 getSmsMessageForLocale(message.messageType, message.locale),
                 phoneNumber
-            )
+            ).catch(e => new MessagingOutcome(false, e))
         }
 
+        // Wait for all to complete, collate outcomes and return
         return Promise.all([pnPromise, smsPromise, emailPromise]).then(values => {
             let pnOutcome = values[0];
             let emailOutcome = values[1];
@@ -112,32 +127,39 @@ function sendToTopic(
 
     return new Promise(resolve => {
         fcm.sendToTopic(topic, payload).then(messagingResponse => {
-            if (messagingResponse) {
-                resolve(new MessagingOutcome(true, undefined))
-            } else {
-                resolve(new MessagingOutcome(false, "fcm.sendToTopic returned undefined"))
-            }
+            resolve(new MessagingOutcome(true, undefined))
         }).catch(e => {
             resolve(new MessagingOutcome(false, e))
         })
     });
 }
 
+/**
+ * Sends an email to the provided address with the provided subject and body.
+ * @param subject The subject of the email.
+ * @param body The body of the email.
+ * @param emailAddress The address to which the email will be sent.
+ */
 function sendEmail(
     subject: string,
     body: string,
     emailAddress: string
 ): Promise<MessagingOutcome> {
-    // NO REJECTIONS! Resolve with a MessagingOutcome instead.
     console.log("sendEmail:", subject, body, emailAddress);
+    // TODO [ndrwksr | 3/19/20] https://github.com/zerobase-io/zerobase_firebase_functions/issues/12
     return Promise.resolve(new MessagingOutcome(true, undefined))
 }
 
+/**
+ * Sends an SMS message with the provided text to the provided phone number.
+ * @param text The text to send in the SMS message.
+ * @param phoneNumber The phone number to which the SMS message will be sent.
+ */
 function sendSms(
     text: string,
     phoneNumber: string
 ): Promise<MessagingOutcome> {
-    // NO REJECTIONS! Resolve with a MessagingOutcome instead.
     console.log("sendSms:", text, phoneNumber);
+    // TODO [ndrwksr | 3/19/20] https://github.com/zerobase-io/zerobase_firebase_functions/issues/13
     return Promise.resolve(new MessagingOutcome(true, undefined))
 }
